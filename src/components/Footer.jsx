b2ef3c311b58c49c242122a691acbfd55887989b
Nischay Chandra <chandranischay@gmail.com>
2022-02-17 17:55:05 +0530

    added footer, filter completed , booking timings

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,69 @@
 import React from 'react'
 import "./footer.css";
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
   return (
     <footer>
-        footer
+        <div className="container">
+          <div className="logo-hold" style={{ maxWidth : "7rem" }}>
+            <img src="./images/bot_main_180.png" alt="Be On Time Logo" width="100%"/>
+          </div>
+          <br />
+          <div className="w-100 d-flex flex-wrap">
+            <div className='col-12 col-md-6 col-lg-4 col-xl-3 border-right'>
+              <br />
+              <h4 className='header'>Reach Us</h4>
+              <div className='text-white'>
+                <p>
+                  <a href='https://goo.gl/maps/rPzqs8vkEcaq9Yj87' target="_blank"> <i className="fas fa-map-marker-alt mr-1"></i> Beside Lenskart Showroom, Link Road, Agrasen Chowk, Bilaspur - 495001 </a>
+                </p>
+                <p>
+                  <a href="mailto:thebeontime@gmail.com" target="_blank"> <i className="fas fa-envelope mr-1"></i> thebeontime@gmail.com </a>
+                </p>
+                <p>
+                  <a href="tel:7566070005"> <i className="fas fa-phone-alt mr-1"></i> +91-7566070005 </a>
+                </p>
+              </div>
+            </div>
+            <div className='col-12 col-md-6 col-lg-2'>
+              <br />
+              <h4 className="header">Sitemap</h4>
+              <div className="text-white">
+                <p>
+                  <Link to="/">Home</Link>
+                </p>
+                <p>
+                  <Link to="/explore">Explore</Link>
+                </p>
+                <p>
+                  <Link to="/terms">Term & Conditions</Link>
+                </p>
+                <p>
+                  <Link to="/about">About Us</Link>
+                </p>
+              </div>
+            </div>
+            <div className='col-12 col-md-12 col-lg-4'>
+              <br />
+              <h4 className="header">Locate</h4>
+              <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d924.3295815196003!2d82.1535722!3d22.0756569!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a280b154f66d34d%3A0xef27e14a67ef3ad1!2sBE%20ON%20TIME%20A%20VALLEY%20OF%20RENTAL%20BIKES!5e0!3m2!1sen!2sin!4v1645099020833!5m2!1sen!2sin" width="100%" height="150" style={{border : "0"}} allowFullScreen={true} loading="lazy"></iframe>
+            </div>
+            <div className='col-12 col-md-12 col-lg-3'>
+              <br />
+              <h4 className="header">Follow Us</h4>
+              <div className="w-100 d-flex" style={{ gap : "1rem" }}>
+                <a href='https://instagram.com/beontimebsp' target="_blank" className="logo instagram">
+                  <i className="fa fa-instagram fa-lg"></i> 
+                </a>
+                <a href="https://www.facebook.com/thebeontime" target="_blank" className="logo facebook">
+                  <i className="fa fa-facebook fa-lg"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+          <hr className='bg-white' />
+        </div>
     </footer>
   )
 }
--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -2,6 +2,12 @@ import React, { useState } from 'react';
 import { useEffect } from 'react';
 import "./explore.css";
 import CONFIG from "../config.json";
+import dayjs from 'dayjs';
+import Timezone from 'dayjs/plugin/timezone';
+import utc from "dayjs/plugin/utc";
+dayjs.extend(utc);
+dayjs.extend(Timezone);
+dayjs.tz.guess()
 
 export default function Explore() {
 
@@ -9,14 +15,16 @@ export default function Explore() {
     const [fullList, setFullList] = useState([]);
     const [query, setQuery] = useState("");
     const [brands, setBrands] = useState([]);
-    const [filter, setFilter] = useState({ type : [], brand : [] });
+    const [filter, setFilter] = useState({ type: [], brand: [] });
+    const [overlay, setOverlay] = useState(false);
+    const [bike, setBike] = useState(null);
 
     useEffect(() => {
         setBikes(CONFIG.bikes);
         setFullList(CONFIG.bikes);
         let distinctBrand = [];
         fullList.forEach(b => {
-            if(!distinctBrand.includes(b.brand)){
+            if (!distinctBrand.includes(b.brand)) {
                 distinctBrand.push(b.brand)
             }
         })
@@ -25,7 +33,7 @@ export default function Explore() {
 
     const onSearchSubmit = (e) => {
         e.preventDefault();
-        if(!query){
+        if (!query) {
             setBikes(fullList);
         } else {
             const regQuery = new RegExp(query, "i");
@@ -36,15 +44,15 @@ export default function Explore() {
 
     const onFilterChange = (e) => {
         const target = e.target;
-        if(target.checked){
+        if (target.checked) {
             const newValues = [...filter[target.name], target.value]
             setFilter(prev => {
-                return { ...prev, [target.name] :  [...newValues]}
+                return { ...prev, [target.name]: [...newValues] }
             })
         } else {
             const newValues = filter[target.name].filter(v => v !== target.value);
             setFilter(prev => {
-                return { ...prev, [target.name] : [...newValues]}
+                return { ...prev, [target.name]: [...newValues] }
             })
         }
     }
@@ -52,26 +60,47 @@ export default function Explore() {
     const onFilterSubmit = (e) => {
         e.preventDefault();
         let filteredList = [];
-        if(filter.brand.length && filter.brand.length){
-            filteredList = fullList.filter(item => (filter.brand.includes(item.brand) && filter.type.includes(item.type)));
+        const brandFilter = filter.brand;
+        const typeFilter = filter.type;
+        if (brandFilter.length && typeFilter.length) {
+            filteredList = fullList.filter(item => (brandFilter.includes(item.brand) && typeFilter.includes(item.type)));
             setBikes(filteredList);
         }
-        
+        else if (brandFilter.length || typeFilter.length) {
+            let key = brandFilter.length ? 'brand' : 'type';
+            filteredList = fullList.filter(item => filter[key].includes(item[key]));
+            setBikes(filteredList);
+        }
+        else {
+            setBikes(fullList);
+        }
+    }
+
+    const onBookNow = (idx) => {
+        setOverlay(true);
+        setBike(bikes[idx]);
+        console.log(bikes[idx].booking);
+    }
+
+    const onBookingChange = (e) => {
+        let newBooking = bike.booking;
+        newBooking[e.target.name] = e.target.value;
+        setBike(prev => ({...prev, newBooking}))
     }
 
     return (
         <main className='explore-main'>
             <div className="container">
-                <form className="search-bar-hold p-1 d-flex" style={{ gap : "2px" }} onSubmit={onSearchSubmit}>
-                    <input type="text" className='form-control' placeholder='Search Bikes' value={query} onChange={(e) => setQuery(e.target.value)} />
+                <form className="search-bar-hold p-1 d-flex justify-content-center" style={{ gap: "5px" }} onSubmit={onSearchSubmit}>
+                    <input type="text" style={{ maxWidth: "700px" }} className='form-control' placeholder='Search Bikes' value={query} onChange={(e) => setQuery(e.target.value)} />
                     <button className='btn btn-warning' type='submit'>
                         <i className="fas fa-search"></i>
                     </button>
                 </form>
                 <br />
             </div>
-                <div className="d-flex flex-wrap justify-content-around">
-                    <div className="filter-box border p-0 col-12 col-sm-11 col-md-8 col-lg-4 col-xl-2">
+            <div className="d-flex flex-wrap justify-content-around">
+                <div className="filter-box p-0 my-2 col-11 col-sm-11 col-md-8 col-lg-4 col-xl-2">
                     <div className="bg-white">
                         <div className="p-2 text-center bg-warning">
                             <span style={{ fontSize: "1.2rem", fontWeight: "600" }}>FILTER</span>
@@ -112,27 +141,75 @@ export default function Explore() {
                             </div>
                         </form>
                     </div>
-                    </div>
-                    <div className="bikes-container d-flex flex-wrap col-12 col-sm-12 col-md-10 col-lg-8 col-xl-10 border">
-                        {
-                            bikes.map((bike, idx) => {
-                                return (
-                                    <div key={idx} className="card-hold my-2 bg-transparent col-12 col-sm-6 col-md-6 col-lg-6 col-xl-3">
-                                        <div className="card">
-                                            <div className="card-img">
-                                                <img src={bike.image} alt={bike.model} width="100%" />
+                </div>
+                <div className="bikes-container d-flex flex-wrap col-12 col-sm-12 col-md-10 col-lg-8 col-xl-10">
+                    {
+                        bikes.map((bike, idx) => {
+                            return (
+                                <div key={idx} className="card-hold my-2 bg-transparent col-12 col-sm-6 col-md-6 col-lg-6 col-xl-3">
+                                    <div className={`card p-0 ${bike.available ? "" : "disabled"}`}>
+                                        <div className="card-img p-0">
+                                            <img src={bike.image} alt={bike.model} width="100%" />
+                                        </div>
+                                        <div className="card-body pb-1 pr-1">
+                                            <h6>{bike.brand} - {bike.model}</h6>
+                                            <div>
+                                                <span>{bike.rent} - {bike.type} - {bike.brand} - {bike.available ? "Y" : "N"}</span>
                                             </div>
-                                            <div className="card-body">
-                                                <h6>{bike.brand} - {bike.model}</h6>
-                                                <p>{bike.rent} - {bike.type} - {bike.brand}</p>
+                                            <div className="d-flex">
+                                                <button className="btn btn-outline-success ml-auto btn-sm" onClick={() => {onBookNow(idx)}}>Book Now</button>
                                             </div>
                                         </div>
                                     </div>
-                                )
-                            })
-                        }
-                    </div>
+                                    {
+                                        !bike.available && <>
+                                            <div className="unavail text-white">
+                                                    <span>
+                                                        This vehicle is booked from
+                                                    </span>
+                                                    <span>
+                                                    {new Date(`${bike.booking.fromDate} ${bike.booking.fromTime}`)
+                                                        .toLocaleString()
+                                                    }
+                                                    </span>
+                                                    <span>
+                                                        to
+                                                    </span>
+                                                    <span>
+                                                    {new Date(`${bike.booking.toDate} ${bike.booking.toTime}`)
+                                                        .toLocaleString()
+                                                    }
+                                                    </span>  
+                                            </div>
+                                        </>
+                                    }
+                                </div>
+                            )
+                        })
+                    }
                 </div>
+            </div>
+            {
+                overlay && <>
+                    <div className="overlay" onClick={() => setOverlay(false)}></div>
+                    <div className="popup-hold">
+                        <div className="card">
+                            <div className="card-body">
+                                    <label className="form-label">Book From</label>
+                                <div className="form-group d-flex">
+                                    <input type="date" name="fromDate" className='form-control' value={bike.booking.fromDate} onChange={onBookingChange} />
+                                    <input type="time" name="fromTime" className='form-control' value={bike.booking.fromTime} onChange={onBookingChange} />
+                                </div>
+                                    <label className="form-label">Book To</label>
+                                <div className="form-group d-flex">
+                                    <input type="date" name="toDate" className='form-control' value={bike.booking.toDate} onChange={onBookingChange} />
+                                    <input type="time" name="toTime" className='form-control' value={bike.booking.toTime} onChange={onBookingChange} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </>
+            }
         </main>
     )
 }
